5ea474ef1b158d875e57527c755a4cf66d38d5be
rileymci <riley.mcilwain@connect.qut.edu.au>
2023-12-12 12:43:53 +1000

    Calculating logic is working correctly

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-var num1;
+var num1 = 0;
 var num2;
 var operator;
-var bool;
+var initialdigit = true;
+var num2string;
+var equalbool = false;
+
+var num2inuse = false;
 
 const displayspan = document.getElementById("dval");
 const calcspan = document.getElementById("dcalc");
@@ -11,31 +15,76 @@ const operateButtons = document.querySelectorAll(".op")
 
 const equalButton = document.querySelector(".eq")
 
+const clearButton = document.querySelector(".clr")
+const deleteButton = document.querySelector(".dlt")
 
 numButtons.forEach(function(button) {
     button.addEventListener("click", function(){
         var number;
         number = button.innerHTML;
         populateDisplay(number);
+        if(operator){
+            num2inuse = true;
+        }
     })
 })
 
 operateButtons.forEach(function(button) {
     button.addEventListener("click", function(){
-        operator = button.innerHTML;
-        displayspan.innerText = calcspan.innerText + " "+ operator;
-        num1 = parseInt(calcspan.innerText);
-        bool = true;
+        if(!num2inuse){
+            operator = button.innerHTML;
+            num1 = parseInt(calcspan.innerText);
+            displayspan.innerText = calcspan.innerText + " "+ operator;
+            initialdigit = true;
+            console.log("1");
+        }
+        //If num2 is in use
+        else{
+            num2 = parseInt(calcspan.innerText);
+            num1 = operate(num1, num2, operator)
+            operator = button.innerHTML;
+            displayspan.innerText = num1.toString() + " "+ operator;
+            calcspan.innerText = num1;
+            num2inuse = false;
+            initialdigit = true;
+        }
+        
+
+        equalbool = false;
     })
 })
 
 
 equalButton.addEventListener("click", function(){
-    num2 = calcspan.innerText;
-    displayspan.innerText =  num1.toString()+ " "+ operator + " " + num2 + " ="; 
-    num2 = parseInt(calcspan.innerText);
-    num1 = operate(num1, num2, operator);
-    calcspan.innerText = num1;
+    if(!equalbool){
+        num2string = calcspan.innerText;
+        displayspan.innerText =  num1.toString()+ " "+ operator + " " + num2string + " =";
+        num2 = parseInt(num2string);
+        num1 = operate(num1, num2, operator);
+        calcspan.innerText = num1;
+
+        equalbool = true;
+    }
+    else{
+        num1 = operate(num1, num2, operator);
+        displayspan.innerText =  calcspan.innerText + " "+ operator + " " + num2string + " =";
+        calcspan.innerText = num1; 
+    }
+
+    initialdigit = true;
+    num2inuse = false;
+})
+
+
+clearButton.addEventListener("click", function(){
+    num1 = 0;
+    num2 = null;
+    initialdigit = true;
+
+    displayspan.innerText = null;
+    calcspan.innerText = "0";
+
+
 })
 
 function operate(num1, num2, operator){
@@ -52,12 +101,13 @@ function operate(num1, num2, operator){
 }
 
 function populateDisplay(number){
-    if(calcspan.innerText == "0" || bool){
+    if(initialdigit){
         calcspan.innerText = number;
-        bool = false;
+        initialdigit = false;
     }
     else{
         calcspan.innerText = calcspan.innerText + number;
     }
 }
+
 
